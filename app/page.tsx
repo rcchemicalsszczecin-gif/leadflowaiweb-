c2dde7f320ecf5a19ceb5a786005f5ee828a374c
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 01:39:06 +0200

    V13.10 cleanup: remove inactive homepage search explainer

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ import {
   ManifestSceneV92,
   PremiumExperienceControllerV92,
 } from "@/components/premium-v9-2-enhancements";
-import { SearchVisibilityExplainerV13 } from "@/components/search-visibility-explainer-v13";
 import { SectionLabel } from "@/components/section-label";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
@@ -130,7 +129,6 @@ export default function HomePage() {
       </section>
 
       <PremiumStageJourneyV9 />
-      <SearchVisibilityExplainerV13 />
       <ManifestSceneV92 />
       <WhatWeBuildV9 />
       <CinematicCoreV92 />
