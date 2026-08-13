1fe649a84d5924445219eb4d067376cbc2d8a405
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-13 11:30:53 +0200

    style: mount V9 premium polish layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import "./interactive-v7.css";
 import "./premium-composition-v8.css";
 import "./premium-composition-v8-themes.css";
 import "./premium-art-direction-v9.css";
+import "./premium-art-direction-v9-polish.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(site.url),
