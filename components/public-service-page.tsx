a68c1ada3e5d042b02e99a589b0c27cfdcaea438
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 01:36:01 +0200

    V13.10 cleanup: remove duplicate service normalization

--- components/public-service-page.tsx
+++ components/public-service-page.tsx
@@ -1,7 +1,6 @@
 import { ServicePage } from "@/components/service-page";
-import { toPublicServicePage } from "@/lib/public-service-page";
 import type { ServicePageData } from "@/lib/services";
 
 export function PublicServicePage({ page }: { page: ServicePageData }) {
-  return <ServicePage page={toPublicServicePage(page)} />;
+  return <ServicePage page={page} />;
 }
