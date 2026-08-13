68c2e611090f6ee49aaaf89d6d96de55ba0117a2
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-13 13:02:57 +0200

    chore: mount responsive performance QA layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ import "./premium-composition-v8-themes.css";
 import "./premium-art-direction-v9.css";
 import "./premium-art-direction-v9-polish.css";
 import "./premium-calibration-v9-2.css";
+import "./responsive-performance-v10.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(site.url),
