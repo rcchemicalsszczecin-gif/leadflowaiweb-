1274bdd3e086ab5c3795217b18590f38954ddc11
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 03:08:13 +0200

    V14.6: mount process and quality canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { V14DeviceTheater } from "@/components/v14-device-theater";
 import { V14Hero } from "@/components/v14-hero";
 import { V14LiquidConstructor } from "@/components/v14-liquid-constructor";
+import { V14ProcessCanvas } from "@/components/v14-process-canvas";
 import { V14SearchTrinity } from "@/components/v14-search-trinity";
 import { V14Services } from "@/components/v14-services";
 
@@ -12,6 +13,7 @@ export default function HomePage() {
       <V14DeviceTheater />
       <V14LiquidConstructor />
       <V14SearchTrinity />
+      <V14ProcessCanvas />
     </main>
   );
 }
