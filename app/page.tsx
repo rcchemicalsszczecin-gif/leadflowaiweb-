015bd98f57208b1925d2d0b21750a24ea6fa2b29
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 03:21:05 +0200

    V14.7: mount first-party portfolio stage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { V14DeviceTheater } from "@/components/v14-device-theater";
 import { V14Hero } from "@/components/v14-hero";
 import { V14LiquidConstructor } from "@/components/v14-liquid-constructor";
+import { V14Portfolio } from "@/components/v14-portfolio";
 import { V14ProcessCanvas } from "@/components/v14-process-canvas";
 import { V14SearchTrinity } from "@/components/v14-search-trinity";
 import { V14Services } from "@/components/v14-services";
@@ -14,6 +15,7 @@ export default function HomePage() {
       <V14LiquidConstructor />
       <V14SearchTrinity />
       <V14ProcessCanvas />
+      <V14Portfolio />
     </main>
   );
 }
