fc4ec6b4c42c9a87c691f497282bad1973a17d76
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 12:41:44 +0200

    V14.9: scope interactive V7 styles to Lab

---
+++ app/lab/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "../interactive-v7.css";
+
+export default function LabLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return children;
+}
