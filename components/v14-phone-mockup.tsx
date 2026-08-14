ae3185cf46ae7afa2c2474a80d351404422514bd
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 02:17:06 +0200

    V14.0: add mobile product mockup

---
+++ components/v14-phone-mockup.tsx
@@ -0,0 +1,11 @@
+export function V14PhoneMockup() {
+  return (
+    <div className="v14-phone" aria-hidden="true">
+      <div className="v14-phone-speaker" />
+      <span>LF</span>
+      <small>MOBILE PRODUCT</small>
+      <strong>Ta sama marka. Inny art direction.</strong>
+      <i /><i /><i />
+    </div>
+  );
+}
