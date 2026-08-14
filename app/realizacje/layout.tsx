9bb4413909b85d01fa260b62c2e84160d0432979
LeadFlowAI <rc.chemicals.szczecin@gmail.com>
2026-08-14 12:45:27 +0200

    V14.9: scope portfolio styles to realizacje

---
+++ app/realizacje/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "../knowledge.css";
+
+export default function PortfolioLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return children;
+}
